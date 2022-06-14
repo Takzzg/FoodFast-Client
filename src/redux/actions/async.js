afda9bddadbc83e78cd34f45ec011cb76ce2a6fc
Guido Queiroz <Takzzg@hotmail.com>
2022-06-14 15:17:49 -0300

    removed extra slash

--- src/redux/actions/async.js
+++ src/redux/actions/async.js
@@ -16,7 +16,7 @@ import {
 
 const baseUrl = `${
     process.env.NODE_ENV === "production"
-        ? "https://food-fast-api.herokuapp.com/"
+        ? "https://food-fast-api.herokuapp.com"
         : "http://localhost:3001"
 }/api/v1`
 
