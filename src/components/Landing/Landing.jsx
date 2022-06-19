e2f639f0bf77937874be66a548500525885493f0
Guido Queiroz <Takzzg@hotmail.com>
2022-06-18 21:26:20 -0300

    categories.all not needed in landing

--- src/components/Landing/Landing.jsx
+++ src/components/Landing/Landing.jsx
@@ -1,24 +1,17 @@
 import React from "react"
-import { useDispatch, useSelector } from "react-redux"
+import { useSelector } from "react-redux"
 import Banner from "./Banner/Banner"
 import CategoryBar from "./UbicationBar/UbicationBar"
 import { CategoriesContainer, GlobalContainer } from "./landingElements"
 import CategoryCard from "../Categories/CategorysLanding"
-import { useEffect } from "react"
-import { searchCategory } from "../../redux/actions/async"
 
 const Landing = () => {
-    const categories = useSelector((state) => state.main.categories.all)
     const filterCategories = useSelector(
         (state) => state.main.categories.filtered
     )
-    const dispatch = useDispatch()
 
-    useEffect(() => {
-        dispatch(searchCategory(""))
-    }, [dispatch])
-
-    const getColor = (i) => `hsl(${(255 / categories.length) * i}, 100%, 33%)`
+    const getColor = (i) =>
+        `hsl(${(255 / filterCategories.length) * i}, 100%, 33%)`
 
     return (
         <GlobalContainer>
@@ -33,23 +26,14 @@ const Landing = () => {
                 <CategoryBar className="LocationBar" />
             </div>
             <CategoriesContainer>
-                {filterCategories.length === 0
-                    ? categories.map((c, i) => (
-                          <CategoryCard
-                              key={c._id}
-                              category={c}
-                              color={() => getColor(i)}
-                              url={`/categories/${c._id}`}
-                          />
-                      ))
-                    : filterCategories.map((c, i) => (
-                          <CategoryCard
-                              key={c._id}
-                              category={c}
-                              color={() => getColor(i)}
-                              url={`/categories/${c._id}`}
-                          />
-                      ))}
+                {filterCategories.map((c, i) => (
+                    <CategoryCard
+                        key={c._id}
+                        category={c}
+                        color={() => getColor(i)}
+                        url={`/categories/${c._id}`}
+                    />
+                ))}
             </CategoriesContainer>
         </GlobalContainer>
     )
