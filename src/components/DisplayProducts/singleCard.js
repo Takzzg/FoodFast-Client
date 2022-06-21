a0ec36c0cd5d0e42fa2692b2fbeb169c0eed5e7b
Guido Queiroz <Takzzg@hotmail.com>
2022-06-20 22:40:11 -0300

    client working (?

--- src/components/DisplayProducts/singleCard.js
+++ src/components/DisplayProducts/singleCard.js
@@ -6,33 +6,33 @@ import {
     FooterContainer
 } from "./displayElements"
 import { MdReadMore } from "react-icons/md"
-import {TbShoppingCartPlus, TbShoppingCartX} from "react-icons/tb"
+import { TbShoppingCartPlus, TbShoppingCartX } from "react-icons/tb"
 import { Link } from "react-router-dom"
 import { baseUrl } from "../../redux/actions/async"
 import { useDispatch, useSelector } from "react-redux"
 import { add_item_car, remove_item_car } from "../../redux/actions/sync"
-import { useLocalStorage } from "../CustomHooks/useLocalStorage"
+// import { useLocalStorage } from "../CustomHooks/useLocalStorage"
 
-export default function SingleProductCard({ product}) {
-    const [isAdded, setIsAdded] = useState(false) 
+export default function SingleProductCard({ product }) {
+    const [isAdded, setIsAdded] = useState(false)
 
-    const dispatch = useDispatch(); 
-    const products = useSelector(state=> state.shopCart.shopCart)
+    const dispatch = useDispatch()
+    const products = useSelector((state) => state.shopCart.shopCart)
     const addItem = (e) => {
-        e.preventDefault(); 
-        const item = {...product, img: {}}
+        e.preventDefault()
+        const item = { ...product, img: {} }
         dispatch(add_item_car(item))
         setIsAdded(true)
     }
-    const removeItem = (e)=> {
-        e.preventDefault(); 
-        const item = {...product, img: {}}
+    const removeItem = (e) => {
+        e.preventDefault()
+        const item = { ...product, img: {} }
         dispatch(remove_item_car(item, true))
         setIsAdded(false)
     }
-    useEffect( ()=> {
-        let coincidence = products.find(el=> el._id === product._id)
-        if(coincidence) setIsAdded(true)
+    useEffect(() => {
+        let coincidence = products.find((el) => el._id === product._id)
+        if (coincidence) setIsAdded(true)
     }, [])
     return (
         <CardContainer>
@@ -40,19 +40,23 @@ export default function SingleProductCard({ product}) {
 
             <ImageContainer>
                 <Link to={`/products/${product._id}`}>
-                    <img src={`${baseUrl}/products/img/${product._id}`} alt="imagen" />
+                    <img
+                        src={`${baseUrl}/products/img/${product._id}`}
+                        alt="imagen"
+                    />
                 </Link>
             </ImageContainer>
 
             <FooterContainer>
-                <PriceContainer>${product.price}</PriceContainer>
-                <ButtonsContainer>
-                    {!isAdded? <TbShoppingCartPlus id="car" onClick={addItem}/>:<TbShoppingCartX id="car" onClick={removeItem}/>}
-
-                    <Link to={`/products/${product._id}`} id="details">
-                        <MdReadMore />
-                    </Link>
-                </ButtonsContainer>
+                ${product.price}
+                {!isAdded ? (
+                    <TbShoppingCartPlus id="car" onClick={addItem} />
+                ) : (
+                    <TbShoppingCartX id="car" onClick={removeItem} />
+                )}
+                <Link to={`/products/${product._id}`} id="details">
+                    <MdReadMore />
+                </Link>
             </FooterContainer>
         </CardContainer>
     )
